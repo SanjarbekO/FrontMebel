845b4fe1e2b8e585109134c964a9f9c7eff6ecdd
SanjarbekO <211amg0707@gmail.com>
2023-06-15 14:56:37 +0600

    front mebel

---
+++ src/components/HitSale/HitSale.jsx
@@ -0,0 +1,41 @@
+import Card from "../Card/Card.jsx";
+import {useContext, useEffect,Fragment} from "react";
+import {CustomContext} from "../../config/context/context.jsx";
+import CardSkeleton from "../CardSkeleton/CardSkeleton";
+
+
+const HitSale = () => {
+
+    const {hitSale, getHitSale,isLoading,} = useContext(CustomContext)
+
+
+    useEffect(() => {
+        getHitSale()
+    },[])
+
+
+
+    return (
+        <section className="hitSale">
+            <div className="container">
+                <h2 className="hitSale__title">
+                    Хиты продаж
+                </h2>
+                <div className="hitSale__row">
+                    {
+                        isLoading ? <CardSkeleton cards={12}/>
+                            :
+                        hitSale.map((item) => (
+                            <Fragment key={item.id}>
+                                <Card item={item}/>
+                            </Fragment>
+
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HitSale;
